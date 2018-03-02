b5ff0d2ae412f88c1b47755474f88ba0616a447a
Amr M. Kayid <amrkayid2027@gmail.com>
2018-03-02 16:38:43 +0200

    Finish occurrences func

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 
 // Initialize app with express
@@ -9,6 +10,23 @@ const _PORT = process.env.PORT;
 app.get('/', (req, res, next) => {
   res.send('Fast Forward Task');
 });
+
+app.get('/count', (req, res, next) => {
+  res.send("Array: " + req.query.array);
+  countOcc(req.query.array);
+});
+
+/* Function to count num of occurences in Arr */
+
+function countOcc(arr) {
+  arr = arr.split(',').map(Number);
+  var occurrences = arr.reduce(function(obj, item) {
+    obj[item] = (obj[item] || 0) + 1;
+    return obj;
+    }, {});
+  console.log(occurrences);
+  return occurrences;
+}
 
 
 // Start the server
